d2e76ef59592eaf47be787db157d0714754a0bc1
digitai <jea.data@gmail.com>
2021-08-13 16:50:55 -0500

    updated npm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  sessionStorage.getItem('token');
+}
+
 function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
 
   if(!token) {
     return <Login setToken={setToken} />
