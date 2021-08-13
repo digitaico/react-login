1396cabd8ba340c52dde8f504690b7b7ade0425c
digitai <jea.data@gmail.com>
2021-08-13 15:10:25 -0500

    created form

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-conmst cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 app.use(cors());
 
+app.use('/login', (req, res) => {
+  res.send({
+    toekn: 'test123'
+  });
+});
 
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
